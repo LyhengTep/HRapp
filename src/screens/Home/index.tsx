ec68a15b2b90c27200b4538e9a2f913c745d2629
Tep Lyheng <tep.lyheng@princebank.com.kh>
2023-01-29 15:10:57 +0700

    initialize project with react native navigation

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Text, View } from "react-native"
+
+
+
+function HomeScreen(){
+    return <View>
+        <Text>Hello screen</Text>
+    </View>
+}
+export default HomeScreen
